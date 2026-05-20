eb1529684ee630222ba5df3f2eccac5f7354eb84
sycamore <gabetastic1313@gmail.com>
2026-05-20 02:50:50 -0400

    Create password.lua

---
+++ E2/official/beta/password.lua
@@ -0,0 +1,17 @@
+@name Password
+@inputs FK1:string FK2:string
+@outputs O1:number O2:number
+
+PW = "Pisslord 18"
+
+if(FK1 == PW) {
+    O1 = 1
+} elseif(!(FK == PW)) {
+    O1 = 0
+}
+
+if(FK2 == PW) {
+    O2 = 1
+} elseif(!(FK2 == PW)) {
+    O2 = 0
+}
