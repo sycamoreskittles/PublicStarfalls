f8e38ac38762d92d652469e78a650e4f49339c6a
sycamore <gabetastic1313@gmail.com>
2026-05-20 02:57:23 -0400

    Create Input Packer: Entity.lua

---
+++ E2/official/utility/packers/Input Packer: Entity.lua
@@ -0,0 +1,5 @@
+@name Input Packer: Entity
+@inputs I1:entity I2:entity I3:entity I4:entity I5:entity I6:entity I7:entity I8:entity I9:entity I10:entity
+@outputs PACKED:array
+@trigger all
+PACKED = array( I1, I2, I3, I4, I5, I6, I7, I8, I9, I10 )
